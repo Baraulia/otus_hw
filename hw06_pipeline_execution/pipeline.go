a0d43e910eb08ed8876fd944ddaec8b9eb1133f5
sergey <baraulia@yandex.ru>
2023-11-11 18:23:36 +0300

    hw06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,41 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	outWithDone := make(Bi)
+
+	go func() {
+		defer close(outWithDone)
+		for value := range in {
+			select {
+			case <-done:
+				return
+			default:
+				outWithDone <- value
+			}
+		}
+	}()
+
+	return buildPipeline(outWithDone, done, stages...)
+}
+
+func buildPipeline(in Out, done In, stages ...Stage) Out {
+	for _, stage := range stages {
+		out := make(Bi)
+		go func(in In, out Bi, done In) {
+			defer close(out)
+			for value := range in {
+				select {
+				case <-done:
+					return
+				default:
+				}
+
+				out <- value
+			}
+		}(in, out, done)
+
+		in = stage(out)
+	}
+
+	return in
 }
