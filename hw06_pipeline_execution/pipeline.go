fc6d27a5edc2aa6716767d81ffd77c847e7feb92
sergey <baraulia@yandex.ru>
2023-11-14 17:20:39 +0300

    hw06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,9 @@
 package hw06pipelineexecution
 
+import (
+	"sync"
+)
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -9,39 +13,25 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	outWithDone := make(Bi)
-
-	go func() {
-		defer close(outWithDone)
-		for value := range in {
-			select {
-			case <-done:
-				return
-			default:
-				outWithDone <- value
-			}
-		}
-	}()
-
-	return buildPipeline(outWithDone, done, stages...)
-}
-
-func buildPipeline(in Out, done In, stages ...Stage) Out {
 	for _, stage := range stages {
 		out := make(Bi)
+		wg := sync.WaitGroup{}
+		wg.Add(1)
 		go func(in In, out Bi, done In) {
 			defer close(out)
+			wg.Done()
 			for value := range in {
 				select {
 				case <-done:
 					return
 				default:
+					out <- value
 				}
-
-				out <- value
 			}
 		}(in, out, done)
 
+		wg.Wait()
+		//time.Sleep(time.Millisecond * 1)
 		in = stage(out)
 	}
 
