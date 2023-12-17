670496e6cee5f36965b8fcf8a6e1280196d4f9be
sergey <baraulia@yandex.ru>
2023-12-17 17:09:24 +0300

    hw10 done

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,15 @@
 package hw10programoptimization
 
 import (
-	"encoding/json"
-	"fmt"
+	"bufio"
+	"bytes"
+	"context"
 	"io"
-	"regexp"
+	_ "net/http/pprof" //nolint:gosec
 	"strings"
+	"sync"
+
+	"github.com/mailru/easyjson"
 )
 
 type User struct {
@@ -18,49 +22,106 @@ type User struct {
 	Address  string
 }
 
+//easyjson:json
+type CustomUser struct {
+	//nolint:tagliatelle
+	Email string `json:"Email"`
+}
+
+const (
+	maxGoroutines = 10
+	bufferSize    = 10
+)
+
 type DomainStat map[string]int
 
+var bufPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
+	rawChan := make(chan *bytes.Buffer, bufferSize)
+	emailChan := make(chan string, bufferSize)
+	errorChan := make(chan error, bufferSize)
+	var wg sync.WaitGroup
+
+	result := make(DomainStat, 1)
+	scanner := bufio.NewScanner(r)
+	scanner.Buffer(make([]byte, 0), 4000)
+	ctx, cancel := context.WithCancel(context.Background())
+	go countDomains(ctx, &wg, emailChan, domain, result)
+
+	for i := 0; i < maxGoroutines; i++ {
+		wg.Add(1)
+		go unmarshall(ctx, &wg, rawChan, emailChan, errorChan)
 	}
-	return countDomains(u, domain)
-}
 
-type users [100_000]User
-
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
-
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
-		var user User
-		if err = json.Unmarshal([]byte(line), &user); err != nil {
-			return
+	wg.Add(1)
+	go func() {
+		defer close(rawChan)
+		defer wg.Done()
+		for scanner.Scan() {
+			b := bufPool.Get().(*bytes.Buffer)
+			b.WriteString(scanner.Text())
+			rawChan <- b
 		}
-		result[i] = user
+	}()
+
+	wg.Wait()
+	wg.Add(1)
+	close(emailChan)
+	close(errorChan)
+	wg.Wait()
+	cancel()
+
+	if len(errorChan) != 0 {
+		err := <-errorChan
+		return nil, err
 	}
-	return
-}
 
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
-		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
-		if err != nil {
-			return nil, err
-		}
-
-		if matched {
-			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
-			num++
-			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
-		}
-	}
 	return result, nil
+}
+
+func countDomains(ctx context.Context, wg *sync.WaitGroup, tasks <-chan string, domain string, result DomainStat) {
+	defer wg.Done()
+	for email := range tasks {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			if strings.Contains(email, "."+domain) {
+				sl := strings.SplitN(email, "@", 2)
+				if len(sl) == 2 {
+					result[strings.ToLower(sl[1])]++
+				}
+			}
+		}
+	}
+}
+
+func unmarshall(
+	ctx context.Context,
+	wg *sync.WaitGroup,
+	tasks <-chan *bytes.Buffer,
+	out chan<- string,
+	errorChan chan error,
+) {
+	defer wg.Done()
+	var user CustomUser
+	for task := range tasks {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			err := easyjson.Unmarshal(task.Bytes(), &user)
+			if err != nil {
+				errorChan <- err
+			}
+			out <- user.Email
+			task.Reset()
+			bufPool.Put(task)
+		}
+	}
 }
