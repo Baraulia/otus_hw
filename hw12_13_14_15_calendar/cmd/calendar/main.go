77a8825fda5545e06826fe255621bd310b65e564
sergey <baraulia@yandex.ru>
2024-01-11 12:47:16 +0300

    hw12 is done

--- hw12_13_14_15_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_calendar/cmd/calendar/main.go
@@ -10,9 +10,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/pkg/logger"
-	"github.com/fixme_my_friend/hw12_13_14_15_calendar/pkg/server/http"
-
 	"github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/app"
 	memorystorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/memory"
 	sqlstorage "github.com/fixme_my_friend/hw12_13_14_15_calendar/internal/storage/sql"
